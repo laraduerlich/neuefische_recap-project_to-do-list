911fa077c2f8b952b04da7bad0ebc75f13ec94e1
laraduerlich <lara.duerlich@t-online.com>
2024-12-16 14:23:25 +0100

    init commit

---
+++ src/main/java/org/example/neuefische_recapproject_todolist/NeuefischeRecapProjectToDoListApplication.java
@@ -0,0 +1,13 @@
+package org.example.neuefische_recapproject_todolist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeuefischeRecapProjectToDoListApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NeuefischeRecapProjectToDoListApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/neuefische_recapproject_todolist/NeuefischeRecapProjectToDoListApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.neuefische_recapproject_todolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeuefischeRecapProjectToDoListApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
