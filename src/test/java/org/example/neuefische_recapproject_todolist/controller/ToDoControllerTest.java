6e8ecee298ed6fcce7d4fc01dbcb9a543ef55f6d
laraduerlich <lara.duerlich@t-online.com>
2025-01-03 11:50:45 +0100

    Add test for ChatGPT API Call

--- src/test/java/org/example/neuefische_recapproject_todolist/controller/ToDoControllerTest.java
+++ src/test/java/org/example/neuefische_recapproject_todolist/controller/ToDoControllerTest.java
@@ -8,11 +8,21 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.client.AutoConfigureMockRestServiceServer;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpMethod;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.test.web.client.MockRestServiceServer;
 import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.client.match.MockRestRequestMatchers.method;
+import static org.springframework.test.web.client.match.MockRestRequestMatchers.requestTo;
+import static org.springframework.test.web.client.response.MockRestResponseCreators.withSuccess;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+import static org.springframework.test.web.client.match.MockRestRequestMatchers.header;
 
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -20,6 +30,7 @@ import static org.junit.jupiter.api.Assertions.*;
 @SpringBootTest
 @AutoConfigureMockMvc
 @DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
+@AutoConfigureMockRestServiceServer
 class ToDoControllerTest {
 
     @Autowired
@@ -28,6 +39,13 @@ class ToDoControllerTest {
     @Autowired
     private ToDoRepo repo;
 
+    @Autowired
+    private MockRestServiceServer mockServer;
+
+    @DynamicPropertySource
+    static void registerProperties(DynamicPropertyRegistry registry) {
+        registry.add("app.chatgpt.api.key", () -> "your-token");
+    }
 
     @Test
     void getAllToDos_shouldReturnEmptyList_whenCalledInitially() throws Exception {
@@ -75,6 +93,49 @@ class ToDoControllerTest {
                                           }
                                           """))
                 .andExpect(jsonPath("$.id").isNotEmpty());
+    }
+
+    @Test
+    void createToDo_checkApiCall () throws Exception{
+        // GIVEN
+        mockServer.expect(requestTo("https://api.openai.com/v1/chat/completions"))
+                .andExpect(method(HttpMethod.POST))
+                .andExpect(header("Authorization", "Bearer your-token"))
+                .andRespond(withSuccess("""
+                        {
+                             "id": "chatcmpl-AlYfXsC7MleTcISu5Tih3xeaJfKdL",
+                             "object": "chat.completion",
+                             "created": 1735898047,
+                             "model": "gpt-3.5-turbo-0125",
+                             "choices": [
+                                 {
+                                     "index": 0,
+                                     "message": {
+                                         "role": "assistant",
+                                         "content": "test",
+                                         "refusal": null
+                                     },
+                                     "logprobs": null,
+                                     "finish_reason": "stop"
+                                 }
+                             ]
+                        }""", MediaType.APPLICATION_JSON));
+        // WHEN & THEN
+        mockMvc.perform(post("/api/todo")
+                        .content("""
+                                {
+                                "description": "teeest",
+                                   "status": "IN_PROGRESS"
+                                }
+                                """)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().json("""
+                                {
+                                    "description": "test",
+                                    "status": "IN_PROGRESS"
+                                }
+                                """));
     }
 
     @Test
@@ -149,4 +210,6 @@ class ToDoControllerTest {
                                 }
                                 """));
     }
+
+
 }
